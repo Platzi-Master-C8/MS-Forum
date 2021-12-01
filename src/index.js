5d9ce99d51822ba9b14da0222020b23337148664
Diego Fernando Mora Velasco <difemove@gmail.com>
2021-11-30 20:15:27 -0500

    feat: added middleware for error handler
    feat: CRUD without SQL connection
    
    docs: discussion endpoints updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+
+const { logErrors, errorHandler } = require('./middlewares/error.handler');
 const routerApi = require('./routes');
 
 const app = express();
 const port = 3001;
 
-
+app.use(express.json())
 routerApi(app);
 
+
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log('App listening on port: ' +  port);
---
+++ src/routes/discussions.router.js
@@ -0,0 +1,54 @@
+const express = require('express');
+
+const router = express.Router();
+const DiscussionsService = require('../services/discussions.service');
+
+const discussionsService = new DiscussionsService();
+
+router.get('/', async (req, res) => {
+    
+  const discussions = await discussionsService.find();
+  res.json(discussions); 
+
+
+});
+
+
+router.get('/:id', async (req, res, next) => {
+    try {
+      const { id } = req.params;
+      const discussion = await discussionsService.findById(parseInt(id));
+      res.json(discussion);
+
+    } catch (error) {
+      next(error);
+    }
+  });
+
+  router.post('/', async (req, res) => {
+    const body = req.body;
+    const discussion = await discussionsService.create(body);
+    res.json(discussion);
+  });
+
+  router.patch('/:id', async (req, res) => {
+    try {
+      const { id } = req.params;
+      const body = req.body;
+      const discussion = await discussionsService.update(parseInt(id), body);
+      res.json(discussion);
+    } catch (error) {
+      res.status(404).json({
+        message: error.message
+      });
+    }
+  
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const discussion = await discussionsService.delete(parseInt(id));
+    res.json(discussion);
+  });
+
+  module.exports = router;
